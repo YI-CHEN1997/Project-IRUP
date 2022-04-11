128215c522cd7b51f15d292217c0e1a47a9537c3
Dupomi <e238499@gmail.com>
2022-04-11 17:38:17 +0800

    add boardview

---
+++ src/components/BusinessCard .vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="cards col-md-5 col-sm-6 px-5 py-4 m-4">
+        <div class="d-flex">
+            <div>
+            <img :src="info.img">
+            </div>
+            <div class="d-flex flex-column justify-content-center m-4">
+                <div class="mb-3">
+                    <h5>{{info.name}}</h5> 
+                    <img src="edit-2-fill.svg" type="button">
+                    <img src="delete-bin-fill.svg" type="button">
+                </div>
+                <div class="contact">
+                    <p><img src="phone.svg"> {{info.phone}}</p>
+                    <p><img src="email.svg"> {{info.email}}</p>
+                </div>
+            </div>
+        </div>
+        <div class="">
+            <p class="text-break my-3">
+                {{info.docs}}
+            </p>
+        </div>
+    </div>
+</template>
+<script>
+
+
+export default {
+    setup() {
+        
+    },
+    props:{ info : Object}
+}
+</script>
+
+<style scoped>
+    .cards:last-child:nth-last-child(2n-1){
+        margin-right: calc(41.6vw + 1.5rem) !important;
+    }
+    .cards{
+        box-shadow: 0 0 30px 1px rgb(220, 220, 220);
+        background-color: #fff;
+    }
+    .contact p{
+        margin-bottom: 0px;
+    }
+    img{
+        border-radius: 99em;
+        max-height: 12vw;
+    }
+</style>
--- src/views/about/BoardView.vue
+++ src/views/about/BoardView.vue
@@ -1,5 +1,53 @@
 <template>
-  <div>
-    
-  </div>
+    <div class="start">
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <div class="title col-5 px-5 py-4 mx-4">
+                    <h2 class="">Executive Board</h2>
+                </div>
+                <div class="">
+                    <button>add</button>
+                </div>
+            </div>
+            <div class="main row justify-content-center">
+                <BusinessCard v-for="info in infos" :key="info" :info="info"></BusinessCard>
+            </div>
+        </div>
+        <div class="bg">
+            <img src="山丘.svg" alt="">
+        </div>
+    </div>
 </template>
+<script>
+import BusinessCard from '@/components/BusinessCard .vue'
+
+export default {
+    setup() {
+        
+    },
+    components:{
+        BusinessCard
+    },
+    data(){
+        return{
+            infos:[{'name':"Ambassador Eric M. Bost",'phone':"979-458-3406",
+            'email':"eric.bost@ag.tamu.edu",
+            'img':"1_Ambassador Eric.M Bost.jpg",
+            'docs':"Deputy Director for Financial Operations & External Relations, Norman Borlaug Institute for International Agriculture Texas A&M University United States"},{'name':"Tim D. Davis",'phone':"979-847-5887",'email':"t-davis5@tamu.edu",'img':"2_Tim D. Davis.jpg",'docs':"Regional Director, Asia Norman Borlaug Institute for International Agriculture Texas A&M University United States"},{'name':'Le Quoc Tuan','phone':'+84-8-3372-0291','email':'quoctuan@hcmuaf.edu.vn','img':'3_Le Quoc Tuan.jpg','docs':'Dean, Faculty of Environment and Natural Resources Nong Lam University Vietnam',},{'name':'','phone':'','email':'','img':'','docs':'',},{'name':'','phone':'','email':'','img':'','docs':'',},{'name':'','phone':'','email':'','img':'','docs':'',},{'name':'','phone':'','email':'','img':'','docs':'',},{'name':'','phone':'','email':'','img':'','docs':'',},{'name':'','phone':'','email':'','img':'','docs':'',}]
+        
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .main{
+
+    }
+    .bg{
+        position: absolute;
+        z-index: -999;
+        top: 500px;
+    }
+
+</style>
