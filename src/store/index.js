695929d80d748a29fd50cc80383aa06419ec9965
Hedgehog Chien <j830813@gmail.com>
2022-04-20 11:41:20 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from "vuex";
 import { db } from "@/firebase/firebaseinit";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
 
 export default createStore({
   state: {
@@ -37,7 +37,7 @@ export default createStore({
   actions: {
     async getBoardMembers({ state }) {
       const dataBase = await collection(db, "executiveBoard");
-      const dbResults = await getDocs(dataBase);
+      const dbResults = await getDocs(query(dataBase, orderBy("profileName")));
       dbResults.docs.forEach((doc) => {
         state.boardMembers.push({ ...doc.data(), id: doc.id });
       });
