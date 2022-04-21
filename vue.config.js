3c8d3b353ea9cfc8350beafca5d8da4a0cf473be
YiChen Wu <92033228+YI-CHEN1997@users.noreply.github.com>
2022-04-21 17:54:19 +0800

    all

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/IRUP-redesign/' // test20200915 為 repo 名稱
+    : '/'
+}
