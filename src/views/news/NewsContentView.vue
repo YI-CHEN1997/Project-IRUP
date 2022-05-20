67d110af01173b05530fb5ff2d9e7e44974e15e4
YiChen Wu <92033228+YI-CHEN1997@users.noreply.github.com>
2022-05-20 14:13:28 +0800

    Update NewsContentView.vue

--- src/views/news/NewsContentView.vue
+++ src/views/news/NewsContentView.vue
@@ -10,6 +10,23 @@
       <div class="content" v-html="currentNews.Content">
 
       </div>
+      <div class="case">
+      <div class="edit-icons d-flex flex-column">
+        <span
+        class="d-flex justify-content-center align-items-center edit-icon"
+        data-bs-toggle="modal"
+        data-bs-target="#EditCaseModal">
+          <i class="fas fa-pen-nib"></i>
+        </span>
+        
+        <span
+        class="d-flex justify-content-center align-items-center delete-icon"
+        data-bs-toggle="modal"
+        data-bs-target="#EditCaseModal">
+          <i class="fas fa-trash"></i>
+        </span>
+      </div>
+    </div>
     </div>
   </main>
 </template>
@@ -93,6 +110,138 @@ export default {
         margin-top: 2rem;
         width: 60%;
       }
+    }
+  }
+}
+
+.case {
+  padding: 0 10vw 5vh 10vw;
+  text-align: center;
+
+  .edit-icons {
+    position: fixed;
+    z-index: 2;
+    right: 4vw;
+    bottom: 4vh;
+  }
+
+  .edit-icon {
+    height: 55px;
+    width: 55px;
+    color: #fff;
+    background-color: #66bb6a;
+    opacity: .8;
+    border-radius: 50%;
+    margin: 0.2rem 0;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #396e3c;
+      scale: 1.2;
+      transition: .2s;
+    }
+
+    svg {
+      height: 20px;
+    }
+
+    @media (max-width: 850px) {
+      height: 40px;
+      width: 40px;
+
+      svg {
+        width: 20px;
+        height: 20px;
+      }
+    }
+  }
+
+  .delete-icon {
+    height: 55px;
+    width: 55px;
+    color: #fff;
+    background-color: #66bb6a;
+    opacity: .9;
+    border-radius: 50%;
+    margin: 0.2rem 0;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #396e3c;
+      scale: 1.2;
+      transition: .2s;
+    }
+
+    svg {
+      height: 25px;
+    }
+
+    @media (max-width: 850px) {
+      height: 40px;
+      width: 40px;
+
+      svg {
+        height: 20px;
+        width: 20px;
+      }
+    }
+  }
+
+  .date {
+    color: #999;
+    margin-bottom: 1rem;
+  }
+
+  img {
+    width: 40%;
+  }
+
+  .content {
+    margin: 0 6rem;
+    text-align: justify;
+
+    h5 {
+      margin: 5rem;
+    }
+
+    h6 {
+      text-align: justify;
+      margin-right: 3rem;
+    }
+
+    img {
+      width: 70%;
+      margin: 2rem 0 1rem 0;
+    }
+  }
+
+  @media (max-width: 850px) {
+    padding: 0 0 1vh 0;
+
+    .content {
+      display: block;
+
+      h3 {
+        margin-bottom: 1rem;
+      }
+
+      h6 {
+        margin-right: 0;
+        text-align: left;
+      }
+
+      li {
+          text-align: left;
+        }
+
+      img {
+        margin-top: 2rem;
+        width: 60%;
+      }
+    }
+
+    .edit-icon, .delete-icon {
+      right: 1rem;
     }
   }
 }
